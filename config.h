c83ac1c8370aa140b76e5ede846c6fbc19cb4080
Mikko Pascual <mikko@artix.localdomain>
2024-10-05 00:07:07 +0200

    Initial commit

---
+++ config.h
@@ -0,0 +1,51 @@
+/* See LICENSE file for copyright and license details. */
+/* Default settings; can be overriden by command line. */
+
+static int topbar = 1;                      /* -b  option; if 0, dmenu appears at bottom     */
+static const unsigned int alpha = 0x99;     /* Amount of opacity. 0xff is opaque             */
+/* -fn option overrides fonts[0]; default X11 font or font set */
+static char font[] = "Iosevka Term:size=10";
+static const char *fonts[] = {
+    font,
+    "NotoColorEmoji:pixelsize=8:antialias=true:autohint=true",
+};
+    /*     fg         bg       */
+
+static char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
+
+static char normfgcolor[] = "#bbbbbb";
+static char normbgcolor[] = "#222222";
+static char selfgcolor[]  = "#eeeeee";
+static char selbgcolor[]  = "#005577";
+static char *colors[SchemeLast][2] = {
+    /*     fg         bg       */
+   [SchemeNorm] = { normfgcolor, normbgcolor },
+   [SchemeSel]  = { selfgcolor,  selbgcolor  },
+   [SchemeOut]  = { "#000000",   "#00ffff" },
+};
+
+static const unsigned int alphas[SchemeLast][2] = {
+    [SchemeNorm] = { OPAQUE, alpha },
+    [SchemeSel] = { OPAQUE, alpha },
+    [SchemeOut] = { OPAQUE, alpha },
+};
+/* -l option; if nonzero, dmenu uses vertical list with given number of lines */
+static unsigned int lines      = 0;
+
+/*
+ * Characters not considered part of a word while deleting words
+ * for example: " /?\"&[]"
+ */
+static const char worddelimiters[] = " ";
+
+/*
+ * Xresources preferences to load at startup
+ */
+ResourcePref resources[] = {
+    { "font",        STRING,  &font },
+    { "color0",      STRING,  &normbgcolor },  // Background for normal items
+    { "color4",      STRING,  &normfgcolor },  // Foreground for normal items
+    { "color0",      STRING,  &selfgcolor },   // Foreground for selected item
+    { "color4",      STRING,  &selbgcolor },   // Background for selected item
+    { "prompt",      STRING,  &prompt },
+};
